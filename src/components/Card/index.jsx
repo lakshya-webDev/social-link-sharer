e56a072115d0a455126267ca343799bcbcb805d3
Lakshya Choudhary <lakshya.webdev@gmail.com>
2024-03-31 16:34:39 +0530

    changes done

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import { Helmet } from "react-helmet";
 import ShareComponent from "../ShareComponent";
 import { Link } from "react-router-dom";
 import { slugify } from "../../utils/helper";
+import { Helmet } from "react-helmet";
+
 const Card = ({ data }) => {
+  const shareUrl = `${window.location.origin}/blog/${slugify(data.title)}`;
+
   return (
     <React.Fragment>
       <Helmet>
@@ -11,27 +14,28 @@ const Card = ({ data }) => {
         <meta property="og:title" content={data.title} />
         <meta property="og:description" content={data.body} />
         <meta property="og:image" content={data.imageUrl} />
-        <meta property="og:url" content={window.location.href} />
+        <meta property="og:url" content={shareUrl} />
       </Helmet>
       <div className="card">
-        <Link to={`blog/${slugify(data.title)}`}>
-          <div className="card-image-wrapper">
-            <img src={data.imageUrl} alt={data.title} />
+        <div className="card-image-wrapper">
+          <img src={data.imageUrl} alt={data.title} />
+        </div>
+        <div className="card-body">
+          <h2 className="tittle">{data.title}</h2>
+          <p>{data.body}</p>
+          <Link to={`blog/${slugify(data.title)}`} className="read-more">
+            Read More..
+          </Link>
+          <div className="card-footer">
+            <h3>Share on:</h3>
+            <ShareComponent
+              url={shareUrl}
+              title={data.title}
+              description={data.body}
+              imageUrl={data.imageUrl}
+            />
           </div>
-          <div className="card-body">
-            <h2 className="tittle">{data.title}</h2>
-            <p>{data.body}</p>
-            <div className="card-footer">
-              <h4>Share on:</h4>
-              <ShareComponent
-                url={window.location.href}
-                title={data.title}
-                description={data.body}
-                imageUrl={data.imageUrl}
-              />
-            </div>
-          </div>
-        </Link>
+        </div>
       </div>
     </React.Fragment>
   );
