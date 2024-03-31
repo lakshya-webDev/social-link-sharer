3f4d402d0f7a016e74885f0737452c5ab4db2f81
Lakshya Choudhary <lakshya.webdev@gmail.com>
2024-03-31 15:09:14 +0530

    Package.json changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.scss";
+import { WebRoutes } from "./routes";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Use ReactDOM.createRoot to render the app
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <WebRoutes />
+  </BrowserRouter>
+);
---
+++ src/routes/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Blogs from "../pages/Blogs";
+import BlogDetailed from "../pages/Blogs/BlogDetailed";
+import { Route, Routes } from "react-router-dom";
+
+export const WebRoutes = () => {
+  const routes = [
+    { path: "/", element: <Blogs /> },
+    { path: "/blog/:slug", element: <BlogDetailed /> },
+  ];
+
+  return (
+    <Routes>
+      {routes.map((route, index) => (
+        <Route key={index} path={route.path} element={route.element} />
+      ))}
+    </Routes>
+  );
+};
